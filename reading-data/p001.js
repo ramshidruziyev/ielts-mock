d01aaaac7fd62c4a3f7f72237da7cca5f686482b
ramshidruziyev <ramshidruziyev079@gmail.com>
2026-01-10 23:01:40 +0500

    Create reading-data/p001.js

---
+++ reading-data/p001.js
@@ -0,0 +1,111 @@
+const passage = {
+  id: "p001",
+  title: "Mental Gymnastics",
+  timeLimit: 20,
+
+  text: `
+<p><b>A.</b> The working day has just started at the head office of Barclays Bank in London. Seventeen staff are helping themselves to a buffet breakfast as young psychologist Sebastian Bailey enters the room to begin the morning's training session. But this is no ordinary training session. He’s not here to sharpen their finance or management skills. He’s here to exercise their brains.</p>
+
+<p><b>B.</b> Today’s workout, organized by a company called the Mind Gym in London, entitled “having presence”. What follows is an intense 90-minute session in which this rather abstract concept is gradually broken down into a concrete set of feelings, mental tricks and behaviors.</p>
+
+<p><b>C.</b> It’s easy to poke fun. Yet similar mental workouts are happening in corporate seminar rooms around the globe. The Mind Gym alone offers some 70 different sessions.</p>
+
+<p><b>D.</b> But whatever the style, the companies’ sales pitch is invariably the same – follow our routines to shape and sculpt your brain or mind, just as you might tone and train your body.</p>
+
+<p><b>E.</b> One outfit, Brainergy of Cambridge, Massachusetts, puts it like this: “Studies have shown that mental exercise can cause changes in brain anatomy and brain chemistry.”</p>
+
+<p><b>F.</b> Indeed, the Mind Gym originally planned to hold its sessions in a local health club, until its founders realized where the real money was to be made.</p>
+
+<p><b>G.</b> Lawrence Katz argues that just as many of us fail to get enough physical exercise, so we also lack sufficient mental stimulation to keep our brain in trim.</p>
+
+<p><b>H.</b> Anything, says Katz, to break your normal mental routine. It will help invigorate your brain, encouraging its cells to make new connections.</p>
+
+<p><b>I.</b> Neurotics, in other words, is about letting your brain fulfill its potential. It cannot create super-brains.</p>
+
+<p><b>J.</b> Guy Claxton dismisses most neurological approaches as “neuron-babble”. Nevertheless, he believes mental skills can be learned.</p>
+  `,
+
+  questions: [
+    {
+      id: 1,
+      type: "ynng",
+      question: "Mind Gym coach instructed employees to imagine that they are the building.",
+      answer: "NO"
+    },
+    {
+      id: 2,
+      type: "ynng",
+      question: "Mind Gym uses similar marketing theory used worldwide.",
+      answer: "YES"
+    },
+    {
+      id: 3,
+      type: "ynng",
+      question: "Susan Greenfield founded Mind Gym.",
+      answer: "NO"
+    },
+    {
+      id: 4,
+      type: "ynng",
+      question: "All industries globally use Mind Gym sessions.",
+      answer: "NO"
+    },
+    {
+      id: 5,
+      type: "ynng",
+      question: "Mind Gym claims strong scientific support for its training.",
+      answer: "NOT GIVEN"
+    },
+
+    {
+      id: 6,
+      type: "match",
+      question: "We do not have enough inspiration to keep our brain fit.",
+      answer: "D"
+    },
+    {
+      id: 7,
+      type: "match",
+      question: "The more you exercise your brain, the more it will grow.",
+      answer: "C"
+    },
+    {
+      id: 8,
+      type: "match",
+      question: "Exercise maintains brain health rather than IQ.",
+      answer: "D"
+    },
+    {
+      id: 9,
+      type: "match",
+      question: "Schools should teach creative thinking skills.",
+      answer: "A"
+    },
+    {
+      id: 10,
+      type: "match",
+      question: "New neural connections can develop after damage.",
+      answer: "D"
+    },
+    {
+      id: 11,
+      type: "match",
+      question: "People label themselves uncreative too early.",
+      answer: "B"
+    },
+    {
+      id: 12,
+      type: "match",
+      question: "Instructor who guided Mind Gym employees.",
+      answer: "B"
+    },
+    {
+      id: 13,
+      type: "match",
+      question: "Most people lack brain skill training.",
+      answer: "A"
+    }
+  ]
+};
+
+export default passage;
