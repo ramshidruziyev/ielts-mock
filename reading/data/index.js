5cf6d0bc09ecaa34d03b8dce5878f6ff4831637d
ramshidruziyev <ramshidruziyev079@gmail.com>
2026-01-11 23:41:51 +0500

    Update index.js

--- reading/data/index.js
+++ reading/data/index.js
@@ -1,29 +1,9 @@
-// reading/data/index.js
-// =======================================
-// READING TESTS INDEX (AUTO)
-// =======================================
-
-// Bu array faqat testlar ro‘yxati uchun
-// Har safar yangi test qo‘shganda
-// faqat shu yerga 1 qator qo‘shasan
-
 window.READING_INDEX = [
   {
     id: "p001",
     title: "Museum Blockbuster",
+    passage: 1,
+    time: 20,
     free: true
-  },
-
-  // Keyin shunday qo‘shib borasan:
-  // {
-  //   id: "p002",
-  //   title: "The History of Ice Cream",
-  //   free: true
-  // },
-  //
-  // {
-  //   id: "p003",
-  //   title: "Adapting to Climate Change",
-  //   free: false
-  // }
+  }
 ];
